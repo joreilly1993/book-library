d955fe2a743a410bfe69d1f978f080c746eff733
John O'Reilly <johnoreilly.cny@gmail.com>
2022-03-21 22:02:59 -0400

    Updated index.html, main.js and styles.css

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.read = read;
     this.info = function() {
         return `${title} by ${author} ${pages} pages, ${read}`;
@@ -13,9 +12,79 @@ function Book(title, author, pages, read) {
     }
 }
 
-function addBookToLibrary() {
 
+// maybe add some form validation.
+// function addBookToLibrary(newBook) {
+    
+//     let newRow = document.createElement('tr');
+//     let newCell = document.createElement('td');
+//     newCell.setAttribute('data-value', myLibrary.length + 1);
+//     let dataValue = newCell.dataset.value;
+//     let delBookBtn = document.createElement('button');
+//     delBookBtn.innerText = 'Remove Book';
+//     table.appendChild(newCell);
+//     table.appendChild(newRow);
+//     table.appendChild(delBookBtn);
+
+//     newCell.innerText = bookTitle.value + ' ' + bookAuthor.value + ' ' + bookStatus.value;
+
+//     myLibrary.push(newBook);
+
+//     console.log(dataValue);
+
+//     delBookBtn.addEventListener('click', () => {
+//         newCell.remove();
+//         newRow.remove();
+//         delBookBtn.remove();
+//         myLibrary.splice(dataValue - 1, 1);
+
+//     })
+// }
+
+//test new function. delete and uncomment one above if does not work
+function addBookToLibrary(newBook) {
+    
+    let newRow = document.createElement('tr');
+    let newCell = document.createElement('td');
+    newCell.setAttribute('data-value', myLibrary.length + 1);
+    let dataValue = newCell.dataset.value;
+    let delBookBtn = document.createElement('button');
+    delBookBtn.innerText = 'Remove Book';
+    table.appendChild(newCell);
+    table.appendChild(newRow);
+    table.appendChild(delBookBtn);
+
+    newRow.innerText = bookTitle.value + ' ' + bookAuthor.value + ' ' + bookStatus.value;
+
+    myLibrary.push(newBook);
+
+    console.log(dataValue);
+
+    delBookBtn.addEventListener('click', () => {
+        newCell.remove();
+        newRow.remove();
+        delBookBtn.remove();
+        myLibrary.splice(dataValue - 1, 1);
+
+    })
 }
+
+
+
+
+
+
+const addBookBtn = document.querySelector('#add-book-btn');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookStatus = document.querySelector('#status');
+const table = document.querySelector('#table');
+
+addBookBtn.addEventListener('click', (title, author, status) => {
+    const newBook = new Book(bookTitle.value, bookAuthor.value, bookStatus.value);
+    
+    addBookToLibrary(newBook);
+})
 
 
 //to do
@@ -30,6 +99,10 @@ function addBookToLibrary() {
 
 
 
-// const Hobbit = new Book('The Hobbit','J.R.R Tolkien', '295', 'not read yet');
+const Hobbit = new Book('The Hobbit','J.R.R Tolkien', '295', 'not read yet');
 
 // const ReadyPlayerOne = new Book('Ready Player One', 'Ernest Cline', '332', 'Read');
+
+
+const tr = document.querySelector('#tdTest');
+const td = document.querySelector('#trTest');
